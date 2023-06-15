534265d86c9333340e90314f1c305a90c5b97347
XXcage <79426486+XXcage@users.noreply.github.com>
2023-06-16 02:47:22 +0300

    Create install-zsh.sh

---
+++ install-zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package information
+sudo apt update
+
+# Install Zsh
+sudo apt install -y zsh
+
+# Install fzf
+sudo apt install -y fzf
+
+# Clone Oh My Zsh
+git clone https://github.com/XXcage/ohmyzsh.git ~/.oh-my-zsh
+
+# Download .zshrc
+curl -sLJO https://raw.githubusercontent.com/XXcage/rz-zsh/main/rz-rcfile && mv rz-rcfile ~/.zshrc
+
+# Clone zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+# Clone zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
+# Set Zsh as the default shell
+chsh -s /usr/bin/zsh
+
+# Print completion message
+echo "Zsh installation complete. Please restart your terminal."
